2a762d5cc10b821715fd7e4cb4e4d6d66e1ad405
ergentus <ponfable999@gmail.com>
2023-09-26 11:10:55 +0300

    HW4

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -18,28 +18,19 @@ export const HW3 = () => {
 
 	return (
 		<div id={'hw03'}>
-			{currentText ? (
-				<h1 id={'hw03-text'}>{currentText}</h1>
-			) : (
-				<h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
-			)}
+			{currentText
+				? <h1 id={'hw03-text'}>{currentText}</h1>
+				: <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1>}
 
 			<input id={'hw03-input'} type="text" value={currentText} onChange={handleChange}/>
-
-			<button id={'hw03-button'} onClick={() => handleSave()}>
-				Сохранить
-			</button>
+			<button id={'hw03-button'} onClick={() => handleSave()}>Сохранить</button>
 
 			<h1 style={{marginTop: '50px'}}>СПИСОК ДЕЛ НА ДЕНЬ:</h1>
-
 			<ol id={'hw03-tasks'}>
-				{texts.map((el, index) => {
-					return (
-						<li key={index} id={`hw03-task-${index}`}>
-							{el}
-						</li>
-					)
-				})}
+				{texts.map((el, index) =>
+					<li key={index} id={`hw03-task-${index}`}>
+						{el}
+					</li>)}
 			</ol>
 		</div>
 	)
